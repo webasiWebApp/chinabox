a0d1ff90eb9eb43a1e5c6b52920527af6b2dd368
webasiWebApp <118072521+webasiWebApp@users.noreply.github.com>
2024-08-25 21:53:46 +0530

    new

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['firebasestorage.googleapis.com'],
-    },
-   
-  };
-  
+  output: 'export',
+  distDir: 'out', // Change this if you prefer another directory name
+  images: {
+    unoptimized: true, // Disable Image Optimization as it's not supported by GitHub Pages
+  },
+  trailingSlash: true, // Ensures all paths are treated as directories
+};
   export default nextConfig;
   
