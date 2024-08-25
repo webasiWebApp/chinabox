b24154676d4ed01499a224612eab3a8f44b44942
webasiWebApp <118072521+webasiWebApp@users.noreply.github.com>
2024-08-25 21:44:58 +0530

    Update PaymentMethod.jsx

--- src/app/(component)/PaymentMethod.jsx
+++ src/app/(component)/PaymentMethod.jsx
@@ -18,6 +18,7 @@ export default function PaymentMethod() {
   const router = useRouter(); // Initialize the router
 
   const handlePaymentSelect = (method) => {
+    
     setSelectedMethod(method);
   };
 
